a8ea027341cf2de319ec024f293f86394583ff16
digiland <sirdiggaz@yahoo.com>
2022-01-16 22:15:27 +0200

    first commit

---
+++ coffee/main.py
@@ -0,0 +1,4 @@
+# makes  3 hit flavours
+# espresso 50ml water 18g coffee beans price = 1.50
+# latte 200ml water 150ml milk 24g coffee beans price = 2.50
+# cappuccino 250ml water 150ml milk 24g coffee beans price = 3.00
